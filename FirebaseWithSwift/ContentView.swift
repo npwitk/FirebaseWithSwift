15688fa65b88b5f27d93ad205f7ec7c5ab18a49b
Nonprawich Intakaew <40120130+npwitk@users.noreply.github.com>
2025-03-21 17:06:57 +0700

    Initial Commit

---
+++ FirebaseWithSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseWithSwift
+//
+//  Created by Nonprawich I. on 21/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseWithSwift/FirebaseWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseWithSwiftApp.swift
+//  FirebaseWithSwift
+//
+//  Created by Nonprawich I. on 21/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
